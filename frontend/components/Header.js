ab54cec330566c909838e1d187fdb6f8987437b1
Corey Lanier <iam@coreylanier.com>
2019-04-03 16:32:11 -0400

    added header

---
+++ frontend/components/Header.js
@@ -0,0 +1,66 @@
+
+import Link from 'next/link';
+import styled from 'styled-components';
+import Router from 'next/router';
+import NProgress from 'nprogress';
+
+Router.onRouteChangeStart = () => {
+    NProgress.start();
+}
+Router.onRouteChangeComplete = () => {
+    NProgress.done();
+}
+Router.onRouteChangeError = () => {
+    NProgress.done();
+}
+
+const Logo = styled.h1`
+    font-size: 4rem;
+    font-family: 'American Captain';
+    margin-left: 2rem;
+    position: relative;
+    z-index: 2;
+    transform: skew(-7deg);
+    a {
+        padding: 0.5rem 1rem;
+        padding-top: 15px;
+        background: ${props => props.theme.action};
+        color: ${props => props.theme.foreground};
+        text-transform: uppercase;
+        text-decoration: none;
+    }
+    @media (max-width: 1300px) {
+        margin: 0;
+        text-align: center;
+    }
+`;
+
+const StyledHeader = styled.header`
+    .bar {
+        border-bottom: 5px solid ${props => props.theme.lightgrey};
+        background: black;
+        display: grid;
+        grid-template-columns: auto 1fr;
+        justify-content: space-between;
+        align-items: stretch;
+        @media (max-width: 1300px) {
+            grid-template-columns: 1fr;
+            justify-content: center;
+            border-bottom: 1px solid ${props => props.theme.lightgrey};
+        }
+    }
+`;
+
+const Header = () => (
+    <StyledHeader>
+        <div className="bar">
+            <Logo>
+                <Link href="/">
+                    <a>NoteZ</a>
+                </Link>
+            </Logo>
+        </div>
+    </StyledHeader>
+)
+
+export default Header;
--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,10 +1,46 @@
 import React, { Component } from 'react';
 import styled, { ThemeProvider, injectGlobal } from 'styled-components';
 import Meta from './Meta';
+import Header from './Header';
 
 const theme = {
-
+    background: '#3c3c3c',
+    foreground: '#f5f5f5',
+    action: '#ff7148',
+    lightgrey: '#E1E1E1',
+    bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)',
 }
+
+injectGlobal`
+    @font-face {
+        font-family: 'American Captain';
+        src: url('/static/amercapt.otf') format('opentype');
+        src: url('/static/amercapt.ttf') format('truetype');
+    }
+  
+    @font-face {
+        font-family: 'Falling Sky';
+        src: url('/static/FallingSky.otf') format('opentype');
+    }
+    html {
+        box-sizing: border-box;
+        font-size: 10px;
+    }
+    *, *:before, *:after {
+        box-sizing: inherit;
+    }
+    body {
+        padding: 0;
+        margin: 0;
+        font-size: 1.5rem;
+        font-family: 'Falling Sky';
+        line-height: 2;
+    }
+    a {
+        text-decoration: none;
+        color: ${theme.foreground};
+    }
+`;
 
 class Page extends Component {
     render() {
@@ -12,7 +48,7 @@ class Page extends Component {
             <ThemeProvider theme={theme}>
                 <div>
                     <Meta />
-
+                    <Header />
                     {this.props.children}
                 </div>
             </ThemeProvider>
