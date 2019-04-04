904dbc5e168f2503ebde29e2fb93d45686b9b1e0
Corey Lanier <iam@coreylanier.com>
2019-04-04 19:23:32 -0400

    created sign-in front-facing mutation

--- frontend/components/Signin.js
+++ frontend/components/Signin.js
@@ -4,6 +4,22 @@ import gql from 'graphql-tag';
 import Form from './styles/Form';
 import Error from './ErrorMessage';
 
+const SIGNIN_MUTATION = gql`
+    mutation SIGNIN_MUTATION(
+        $email: String!
+        $password: String!
+    ) {
+        signin(
+            email: $email
+            password: $password
+        ) {
+            id
+            username
+            email
+        }
+    }
+`
+
 class Signin extends Component {
     state = {
         email: '',
@@ -19,20 +35,35 @@ class Signin extends Component {
 
     render() {
         return(
-            <Form method="post">
-                <fieldset>
-                    <h2>Existing user? RTSD!</h2>
-                    <label htmlFor="email">
-                        Email:
-                        <input type="email" name="email" placeholder="Email address" value={this.state.email} onChange={this.changeState} />
-                    </label>
-                    <label htmlFor="password">
-                        Password:
-                        <input type="password" name="password" placeholder="Passowrd" value={this.state.password} onChange={this.changeState} />
-                    </label>
-                    <button type="submit">Sign In</button>
-                </fieldset>
-            </Form>
+            <Mutation mutation={SIGNIN_MUTATION} variables={this.state}>
+                {(signin, {error, loading}) => {
+                    return (
+                        <Form method="post" onSubmit={async (e) => {
+                            e.preventDefault();
+                            const res = await signin();
+                            console.log(res);
+                            this.setState({
+                                email: '',
+                                password: ''
+                            })
+                        }}>
+                            <fieldset disabled={loading} aria-busy={loading}>
+                                <h2>Existing user? RTSD!</h2>
+                                <Error error={error} />
+                                <label htmlFor="email">
+                                    Email:
+                                    <input type="email" name="email" placeholder="Email address" value={this.state.email} onChange={this.changeState} />
+                                </label>
+                                <label htmlFor="password">
+                                    Password:
+                                    <input type="password" name="password" placeholder="Passowrd" value={this.state.password} onChange={this.changeState} />
+                                </label>
+                                <button type="submit">Sign In</button>
+                            </fieldset>
+                        </Form>
+                    )
+                }}
+            </Mutation>
         )
     }
 }
