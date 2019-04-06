00a1c2316fb7a730c50cd5ca4ec1f841bed8384c
Corey Lanier <iam@coreylanier.com>
2019-04-05 20:32:29 -0400

    created front-facing query for finding games

--- frontend/components/CreateGame.js
+++ frontend/components/CreateGame.js
@@ -1,16 +1,49 @@
 import React, { Component } from 'react';
-import { Mutation } from 'react-apollo';
+import { Query, Mutation, ApolloConsumer } from 'react-apollo';
 import gql from 'graphql-tag';
 import Downshift, {resetIdCounter} from 'downshift';
+import debounce from 'lodash.debounce';
 import User, { CURRENT_USER_QUERY } from './User';
 import Form from './styles/Form';
 import Error from './ErrorMessage';
 
+const SEARCH_GAMES_QUERY = gql`
+    query SEARCH_GAMES_QUERY(
+        $searchTerm: String!
+    ) {
+        games(
+            where: {
+                name_contains: $searchTerm
+            }
+        ) {
+            id
+            name
+        }
+    }
+`;
+
 class CreateGame extends Component {
     state = {
         gameName: '',
+        games: [],
         characterList: []
     }
+
+    onChange = debounce(async (e, client) => {
+        console.log(`searching`);
+        this.setState({
+            loading: true
+        })
+        const res = await client.query({
+            query: SEARCH_GAMES_QUERY,
+            variables: { searchTerm: e.target.value}
+        });
+        const { games } = res.data;
+        this.setState({
+            games,
+            loading: false
+        });
+    }, 350);
 
     changeGame = (e) => {
         
