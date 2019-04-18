49a36001796ba03dc9963d3c36cb655fb32a803f
Corey Lanier <iam@coreylanier.com>
2019-04-18 16:32:04 -0400

    pulling characters to state

--- frontend/components/Games.js
+++ frontend/components/Games.js
@@ -7,9 +7,54 @@ import ReactSelectStyles from './styles/ReactSelectStyles';
 import {ALL_GAMES_QUERY} from './CreateCharacter';
 
 class Games extends Component {
+    state = {
+        game: '',
+        yourCharacter: '',
+        oppCharacter: '',
+        filter: '',
+        characters: []
+    }
+
     render() {
         return(
-            <p>Oh hey it's a note page.</p>
+            <Query query={ALL_GAMES_QUERY}>
+                {({data: {games}}) => (
+                    <>
+                        <h2>Game Notes</h2>
+                        <label htmlFor="game">
+                            Select your game:
+                            <Select name="game" styles={ReactSelectStyles} options={games.map((game) => {
+                                return {
+                                    label: game.name,
+                                    value: game.id
+                                }
+                            })} onChange={(e) => {
+                                const {value} = e;
+                                games.map(async game => {
+                                    if (game.id === value) {
+                                        await this.setState({
+                                            game: value,
+                                            characters: game.characters
+                                        });
+                                    }
+                                });
+                            }} />
+                        </label>
+                        <label htmlFor="yourCharacter">
+                            Your Character:
+                            <Select name="yourCharacter" styles={ReactSelectStyles} />
+                        </label>
+                        <label htmlFor="oppCharacter">
+                            Opponent's Character:
+                            <Select name="oppCharacter" styles={ReactSelectStyles} />
+                        </label>
+                        <label htmlFor="filter">
+                            Filter By:
+                            <Select name="filter" styles={ReactSelectStyles} />
+                        </label>
+                    </>
+                )}
+            </Query>
         )
     }
 }
---
+++ frontend/pages/players.js
@@ -0,0 +1,13 @@
+import PleaseSignIn from '../components/PleaseSignIn';
+
+const GameNotesPage = (props) => {
+    return( 
+        <div>
+            <PleaseSignIn>
+                <p>Here's where player notes will go.</p>
+            </PleaseSignIn>
+        </div>
+    )
+}
+
+export default GameNotesPage;
