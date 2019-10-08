de5f37adb55e7c15d32fc24c35a070618e6b6699
Corey <iam@coreylanier.com>
2019-10-07 22:54:59 -0400

    created login

--- new/src/App.js
+++ new/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import axios from "axios";
 import Header from "./components/Header";
 import Login from "./components/Login";
 import "./App.css";
@@ -9,18 +10,44 @@ class App extends React.Component {
     user: null,
     role: null
   };
-  changeState = e => {
-    const { name, value } = e.target;
+
+  componentDidMount = async () => {
+    const user = localStorage.getItem("notezId");
     this.setState({
-      [name]: value
+      user
     });
+    await this.checkRole();
   };
+
+  setUser = user => {
+    this.setState({
+      user
+    });
+    this.checkRole();
+  };
+
+  checkRole = async () => {
+    const { user } = this.state;
+    if (user) {
+      const userData = await axios.get(`/api/users/${user}`);
+      const { role } = userData.data.data;
+      this.setState({
+        role
+      });
+    }
+  };
+
   render() {
     return (
       <div className="App">
         <Router>
           <Header user={this.state.user} role={this.state.role} />
-          <Route path="/login" component={() => <Login />} />
+          <main>
+            <Route
+              path="/login"
+              component={() => <Login setUser={this.setUser} />}
+            />
+          </main>
         </Router>
       </div>
     );
--- new/src/components/Login.js
+++ new/src/components/Login.js
@@ -1,15 +1,89 @@
 import React from "react";
 import axios from "axios";
+import { Redirect } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faKey } from "@fortawesome/free-solid-svg-icons";
+import { setToken } from "../services/tokenService";
 
 export default class Login extends React.Component {
   state = {
     email: "",
-    password: ""
+    password: "",
+    loading: false,
+    success: false,
+    error: null
   };
-  render(props) {
+
+  changeState = e => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  login = async e => {
+    e.preventDefault();
+    this.setState({
+      loading: true
+    });
+    const { email, password } = this.state;
+    try {
+      const res = await axios.post("/api/users/login", {
+        data: {
+          email,
+          password
+        }
+      });
+      if (res) {
+        const { token, id } = res.data.data;
+        await setToken(token);
+        await localStorage.setItem("notezId", id);
+        await this.props.setUser(id);
+        this.setState({
+          loading: false,
+          success: true
+        });
+      }
+    } catch (e) {
+      this.setState({
+        error: e,
+        loading: false
+      });
+    }
+  };
+
+  render() {
+    if (this.state.success) {
+      return <Redirect to="/" />;
+    }
     return (
       <section className="login">
-        <p>Login</p>
+        <h2>Login</h2>
+        <form disabled={this.state.loading} onSubmit={this.login}>
+          <fieldset aria-busy={this.state.loading}>
+            <label htmlFor="email">
+              <FontAwesomeIcon icon={faEnvelope} />
+              <input
+                type="text"
+                name="email"
+                value={this.state.email}
+                onChange={this.changeState}
+                placeholder="Email Address"
+              />
+            </label>
+            <label htmlFor="password">
+              <FontAwesomeIcon icon={faKey} />
+              <input
+                type="password"
+                name="password"
+                value={this.state.password}
+                onChange={this.changeState}
+                placeholder="Password"
+              />
+            </label>
+            <button type="submit">Login</button>
+          </fieldset>
+        </form>
       </section>
     );
   }
