f74c8d3653c00f8dc21a7472696272bdff4f3a6e
MissingPersonFGC <iam@coreylanier.com>
2019-11-14 19:13:09 -0500

    working on main page prior to login

--- src/components/Attract.js
+++ src/components/Attract.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { Container, Typography, Grid } from "@material-ui/core";
+import logo from "../assets/logo.png";
 
 function Attract() {
-  return <h1>Welcome to NoteZ</h1>;
+  return (
+    <Container>
+      <Grid container spacing={2}>
+        <Grid item md={5} xs={12}>
+          <img className="main-logo" src={logo} alt="NoteZ" />
+        </Grid>
+        <Grid item md={7} xs={12}>
+          <Typography variant="h3">Welcome to NoteZ</Typography>
+          <Typography variant="h5">
+            Your premiere fighting game note app!
+          </Typography>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 }
 
 export default Attract;
