dde90aa4f37883462749f5d56d2cb7fe19047dbe
MissingPersonFGC <iam@coreylanier.com>
2019-11-21 23:39:03 -0500

    preparing to list roles on page upon selection

--- src/components/UserSettings.js
+++ src/components/UserSettings.js
@@ -1,5 +1,7 @@
 import React from "react";
 import axios from "axios";
+import Select from "react-select";
+import { Container, Button, Typography } from "@material-ui/core";
 import { getToken } from "../services/tokenService";
 
 class UserSettings extends React.Component {
@@ -14,9 +16,7 @@ class UserSettings extends React.Component {
 
   async componentWillMount() {
     const token = getToken();
-    console.log(token);
     const { user } = this.props;
-    console.log(user);
     await axios
       .get("/api/users", {
         params: {
@@ -31,8 +31,34 @@ class UserSettings extends React.Component {
       });
   }
 
+  setUser = e => {
+    const id = e.value;
+    const { users } = this.state;
+    const index = users.findIndex(user => user._id === id);
+    const { role } = users[index];
+    this.setState({
+      id,
+      role
+    });
+  };
+
   render() {
-    return <h1>User Settings</h1>;
+    return (
+      <section>
+        <Container maxWidth="sm">
+          <Typography variant="h5">Edit User Roles</Typography>
+          <Select
+            options={this.state.users.map(user => {
+              return {
+                label: `${user.username} (${user.realName} - ${user.country})`,
+                value: user._id
+              };
+            })}
+            onChange={this.setUser}
+          />
+        </Container>
+      </section>
+    );
   }
 }
 
