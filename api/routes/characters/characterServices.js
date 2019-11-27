4d253269d7d438f94e9d8a87a8867bf568a071ec
MissingPersonFGC <iam@coreylanier.com>
2019-11-27 17:39:32 -0500

    created endpoint for updating characters

--- api/routes/characters/characterServices.js
+++ api/routes/characters/characterServices.js
@@ -19,3 +19,31 @@ exports.getAllCharacters = async () => {
     throw e;
   }
 };
+
+exports.updateCharacter = async (
+  id,
+  name,
+  name_ja,
+  name_ko,
+  name_cn,
+  name_tw,
+  name_hk
+) => {
+  try {
+    return await Character.findByIdAndUpdate(
+      { _id: id },
+      {
+        $set: {
+          name,
+          name_ja,
+          name_ko,
+          "name_zh-cn": name_cn,
+          "name_zh-tw": name_tw,
+          "name_zh-hk": name_hk
+        }
+      }
+    );
+  } catch (e) {
+    throw e;
+  }
+};
