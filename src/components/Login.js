16b36c5a15b3bc5402c0482f208d36e2af5a8f73
Corey Lanier <iam@coreylanier.com>
2020-01-25 21:41:49 -0500

    refactored Login component to use user/language context

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,26 +1,25 @@
-import React from "react";
-import axios from "axios";
+import React, { useContext, useState } from "react";
 import { Redirect, Link as RouterLink } from "react-router-dom";
-import PropTypes from "prop-types";
-import { withStyles } from "@material-ui/core/styles";
 import {
   TextField,
   Button,
   Typography,
   Container,
-  CircularProgress
+  CircularProgress,
+  makeStyles
 } from "@material-ui/core";
 import {
   login,
   goBack,
-  email,
-  password,
+  email as emailLocale,
+  password as passwordLocale,
   forgotYourPassword
 } from "../data/locales";
 import localeSelect from "../services/localeSelect";
-import { setToken } from "../services/tokenService";
+import { language } from "../contexts/LanguageContext";
+import { UserContext } from "../contexts/UserContext";
 
-const styles = theme => ({
+const useStyles = makeStyles(theme => ({
   container: {
     display: "flex",
     flexWrap: "wrap"
@@ -44,147 +43,99 @@ const styles = theme => ({
     marginTop: -12,
     marginLeft: -12
   }
-});
+}));
 
-class Login extends React.Component {
-  state = {
-    email: "",
-    password: "",
-    loading: false,
-    success: false,
-    error: null
-  };
-
-  changeState = e => {
-    const { name, value } = e.target;
-    this.setState({
-      [name]: value
-    });
-  };
-
-  login = async e => {
-    e.preventDefault();
-    this.setState({
-      loading: true,
-      error: null
-    });
-    const { email, password } = this.state;
-    try {
-      const res = await axios.post("/api/users/login", {
-        data: {
-          email,
-          password
-        }
-      });
-      if (res) {
-        const { token, id } = res.data.data;
-        await setToken(token);
-        await localStorage.setItem("notezId", id);
-        await this.setState({
-          success: true,
-          loading: false
-        });
-        await this.props.setUser(id);
-      }
-    } catch (e) {
-      this.setState({
-        error: e.message,
-        loading: false
-      });
-    }
-  };
-
-  render() {
-    if (this.state.success) {
-      return <Redirect to="/" />;
-    }
-    const { classes } = this.props;
-    return (
-      <section>
-        <Container maxWidth="xs">
-          <Typography className={classes.header} variant="h5">
-            {localeSelect(this.props.language, login)}
-          </Typography>
-          <form
-            disabled={this.state.loading}
-            onSubmit={this.login}
-            className={classes.container}
-          >
+export default function Login() {
+  const classes = useStyles();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { doLogin, loading, error, success } = useContext(UserContext);
+  if (success) {
+    return <Redirect to="/" />;
+  }
+  return (
+    <section>
+      <Container maxWidth="xs">
+        <Typography className={classes.header} variant="h5">
+          {localeSelect(language, login)}
+        </Typography>
+        <form
+          disabled={loading}
+          onSubmit={e => {
+            e.preventDefault();
+            doLogin(email, password);
+          }}
+          className={classes.container}
+        >
+          <Container maxWidth="xs">
+            {error && (
+              <p className="error">
+                <span>Error:</span> {error}
+              </p>
+            )}
             <Container maxWidth="xs">
-              {this.state.error && (
-                <p className="error">
-                  <span>Error:</span> {this.state.error}
-                </p>
-              )}
-              <Container maxWidth="xs">
-                <TextField
-                  label={localeSelect(this.props.language, email)}
-                  id="standard-name"
-                  value={this.state.email}
-                  name="email"
-                  onChange={this.changeState}
-                  fullWidth
-                />
-              </Container>
-              <Container maxWidth="xs">
-                <TextField
-                  label={localeSelect(this.props.language, password)}
-                  value={this.state.password}
-                  id="standard-password-input"
-                  type="password"
-                  name="password"
-                  onChange={this.changeState}
-                  fullWidth
-                />
-              </Container>
-              <Container className={classes.buttonRow}>
-                <div className={classes.wrapper}>
-                  <Button
-                    variant="contained"
-                    type="submit"
-                    color="primary"
-                    onClick={this.login}
-                    disabled={this.state.loading}
-                  >
-                    {localeSelect(this.props.language, login)}
-                  </Button>
-                  {this.state.loading && (
-                    <CircularProgress
-                      size={20}
-                      color="secondary"
-                      className={classes.buttonProgress}
-                    />
-                  )}
-                </div>
-                <div className={classes.wrapper}>
-                  <Button
-                    component={React.forwardRef((props, ref) => (
-                      <RouterLink innerRef={ref} to="/" {...props} />
-                    ))}
-                  >
-                    {localeSelect(this.props.language, goBack)}
-                  </Button>
-                </div>
-              </Container>
-              <Container className={classes.buttonRow}>
+              <TextField
+                label={localeSelect(language, emailLocale)}
+                id="standard-name"
+                value={email}
+                onChange={e => {
+                  setEmail(e.target.value);
+                }}
+                fullWidth
+              />
+            </Container>
+            <Container maxWidth="xs">
+              <TextField
+                label={localeSelect(language, passwordLocale)}
+                value={password}
+                id="standard-password-input"
+                type="password"
+                onChange={e => {
+                  setPassword(e.target.value);
+                }}
+                fullWidth
+              />
+            </Container>
+            <Container className={classes.buttonRow}>
+              <div className={classes.wrapper}>
+                <Button
+                  variant="contained"
+                  type="submit"
+                  color="primary"
+                  disabled={this.state.loading}
+                >
+                  {localeSelect(language, login)}
+                </Button>
+                {this.state.loading && (
+                  <CircularProgress
+                    size={20}
+                    color="secondary"
+                    className={classes.buttonProgress}
+                  />
+                )}
+              </div>
+              <div className={classes.wrapper}>
                 <Button
                   component={React.forwardRef((props, ref) => (
-                    <RouterLink innerRef={ref} to="/forgot" {...props} />
+                    <RouterLink innerRef={ref} to="/" {...props} />
                   ))}
                 >
-                  {localeSelect(this.props.language, forgotYourPassword)}
+                  {localeSelect(language, goBack)}
                 </Button>
-              </Container>
+              </div>
             </Container>
-          </form>
-        </Container>
-      </section>
-    );
-  }
+            <Container className={classes.buttonRow}>
+              <Button
+                component={React.forwardRef((props, ref) => (
+                  <RouterLink innerRef={ref} to="/forgot" {...props} />
+                ))}
+              >
+                {localeSelect(language, forgotYourPassword)}
+              </Button>
+            </Container>
+          </Container>
+        </form>
+      </Container>
+    </section>
+  );
 }
-
-Login.propTypes = {
-  classes: PropTypes.object.isRequired
-};
-
-export default withStyles(styles)(Login);
--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -1,12 +1,15 @@
 import React, { createContext, useState, useEffect } from "react";
 import axios from "axios";
-import { removeToken } from "../services/tokenService";
+import { removeToken, setToken } from "../services/tokenService";
 
 export const UserContext = createContext();
 
 const UserContextProvider = props => {
   const [user, setUser] = useState(null);
   const [role, setRole] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [success, setSuccess] = useState(true);
 
   useEffect(() => {
     const userId = localStorage.getItem("notezId") || null;
@@ -25,11 +28,37 @@ const UserContextProvider = props => {
     removeToken();
     localStorage.removeItem("notezId");
     setUser(null);
-    setRole(null);
+  };
+
+  const doLogin = async (email, password) => {
+    setLoading(true);
+    setError(null);
+
+    try {
+      const res = await axios.post("/api/users/login", {
+        data: {
+          email,
+          password
+        }
+      });
+      if (res) {
+        const { token, id } = res.data.data;
+        setToken(token);
+        localStorage.setItem("notezId", id);
+        setUser(id);
+        setLoading(false);
+        setSuccess(true);
+      }
+    } catch (e) {
+      setError(e.message);
+      setLoading(false);
+    }
   };
 
   return (
-    <UserContext.Provider value={{ user, role, logout }}>
+    <UserContext.Provider
+      value={{ user, role, logout, doLogin, error, success, loading }}
+    >
       {props.children}
     </UserContext.Provider>
   );
