a450c631aaa2bac05b137fd202a731223dad7bd8
Corey Lanier <iam@coreylanier.com>
2020-04-18 23:08:47 -0400

    deleted commented code

--- api/_routes/invites/inviteRoutes.js
+++ api/_routes/invites/inviteRoutes.js
@@ -25,29 +25,6 @@ router.route("/").post(async (req, res) => {
   } catch (e) {
     res.status(401).send(e);
   }
-  // const { email } = req.body.data;
-  // try {
-  //   Promise.all(async () => {
-  //     // check the user database for an existing user with the email address.
-  //     const user = await userService.findUser(email);
-  //     // if they exist, throw error
-  //     if (user) {
-  //       res
-  //         .status(401)
-  //         .send({ e: { message: "A user with this email already exists." } });
-  //     }
-  //     // if it doesn't exist, save invite to invite collection.
-  //     const newInvite = {
-  //       email,
-  //     };
-  //     const finished = await inviteService.createInvite(newInvite);
-  //     // send email invite using id as link.
-
-  //     res.status(201);
-  //   });
-  // } catch (e) {
-  //   res.status(401).send(e);
-  // }
 });
 
 router.route("/:id").get(async (req, res) => {
