b51202dda7c02c7908c6a68467238c05e67f541a
Corey Lanier <iam@coreylanier.com>
2021-10-11 22:08:58 -0400

    create translation json for en i18n

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
@@ -6,13 +6,17 @@ import UserContextProvider from "./contexts/UserContext";
 import LanguageContextProvider from "./contexts/LanguageContext";
 import * as serviceWorker from "./serviceWorker";
 
+import "./i18n";
+
 ReactDOM.render(
-  <LanguageContextProvider>
-    <UserContextProvider>
-      <App />
-    </UserContextProvider>
-  </LanguageContextProvider>,
-  document.getElementById("root")
+	<Suspense fallback="loading">
+		<LanguageContextProvider>
+			<UserContextProvider>
+				<App />
+			</UserContextProvider>
+		</LanguageContextProvider>
+	</Suspense>,
+	document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
