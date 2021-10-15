ce544b0c983677e30534aa18de6f7c8c2d9e10c4
Corey Lanier <iam@coreylanier.com>
2021-10-15 00:49:02 -0400

    refactor game note search to use react-i18next

--- src/components/GameNotesSearch.js
+++ src/components/GameNotesSearch.js
@@ -3,121 +3,108 @@ import { Typography, Button, makeStyles } from "@material-ui/core";
 import Select from "react-select";
 import { LanguageContext } from "../contexts/LanguageContext";
 import { GameContext } from "../contexts/GameContext";
-import localeSelect from "../services/localeSelect";
 import dbLocale from "../services/dbLocale";
-import {
-  chooseGame,
-  yourCharacter as yourCharacterLocale,
-  opponentCharacter as opponentCharacterLocale,
-  chooseFilter,
-  clearFilter,
-} from "../data/locales";
 import { NoteContext } from "../contexts/NoteContext";
+import { useTranslation } from "react-i18next";
 
 const useStyles = makeStyles((theme) => ({
-  spaced: {
-    marginBottom: theme.spacing(2),
-  },
+	spaced: {
+		marginBottom: theme.spacing(2),
+	},
 }));
 
 export default function GameNotesSearch() {
-  const classes = useStyles();
-  const { language } = useContext(LanguageContext);
-  const { games, updateDropdowns, characters, filters } = useContext(
-    GameContext
-  );
-  const {
-    gameNotesGame: game,
-    setGameNotesGame: setGame,
-    setMyCharacter,
-    setOpponentCharacter,
-    gameNotesFilter: myFilter,
-    setGameNotesFilter: setMyFilter,
-    setDisplayedGameNotes: setDisplayedNotes,
-  } = useContext(NoteContext);
+	const { t } = useTranslation();
+	const classes = useStyles();
+	const { language } = useContext(LanguageContext);
+	const { games, updateDropdowns, characters, filters } =
+		useContext(GameContext);
+	const {
+		gameNotesGame: game,
+		setGameNotesGame: setGame,
+		setMyCharacter,
+		setOpponentCharacter,
+		gameNotesFilter: myFilter,
+		setGameNotesFilter: setMyFilter,
+		setDisplayedGameNotes: setDisplayedNotes,
+	} = useContext(NoteContext);
 
-  // Effect called to grab characters and filters from chosen game, and set them to state.
-  useEffect(() => {
-    if (game !== "") {
-      setMyCharacter("");
-      setMyFilter("");
-      setOpponentCharacter("");
-      setDisplayedNotes([]);
-      updateDropdowns(game, "game");
-    }
-  }, [game, games, language]);
+	// Effect called to grab characters and filters from chosen game, and set them to state.
+	useEffect(() => {
+		if (game !== "") {
+			setMyCharacter("");
+			setMyFilter("");
+			setOpponentCharacter("");
+			setDisplayedNotes([]);
+			updateDropdowns(game, "game");
+		}
+	}, [game, games, language]);
 
-  return (
-    <>
-      <Typography variant="h6">{localeSelect(language, chooseGame)}</Typography>
-      <Select
-        options={games.map((game) => {
-          return {
-            label: dbLocale(language, game),
-            value: game._id,
-          };
-        })}
-        onChange={(e) => {
-          setGame(e.value);
-        }}
-        className={classes.spaced}
-      />
-      <Typography variant="h6">
-        {localeSelect(language, yourCharacterLocale)}
-      </Typography>
-      <Select
-        options={characters.map((character) => {
-          return {
-            label: dbLocale(language, character),
-            value: character._id,
-          };
-        })}
-        onChange={(e) => {
-          setMyCharacter(e.value);
-        }}
-        className={classes.spaced}
-      />
-      <Typography variant="h6">
-        {localeSelect(language, opponentCharacterLocale)}
-      </Typography>
-      <Select
-        options={characters.map((character) => {
-          return {
-            label: dbLocale(language, character),
-            value: character._id,
-          };
-        })}
-        onChange={(e) => {
-          setOpponentCharacter(e.value);
-        }}
-        className={classes.spaced}
-      />
-      <Typography variant="h6">
-        {localeSelect(language, chooseFilter)}
-      </Typography>
-      <Select
-        options={filters.map((x) => {
-          return {
-            label: dbLocale(language, x),
-            value: x._id,
-          };
-        })}
-        onChange={(e) => {
-          setMyFilter(e.value);
-        }}
-        className={classes.spaced}
-      />
-      {myFilter !== "" && (
-        <Button
-          variant="outlined"
-          color="secondary"
-          onClick={() => {
-            setMyFilter("");
-          }}
-        >
-          {localeSelect(language, clearFilter)}
-        </Button>
-      )}
-    </>
-  );
+	return (
+		<>
+			<Typography variant="h6">{t("notes.common.game")}</Typography>
+			<Select
+				options={games.map((game) => {
+					return {
+						label: dbLocale(language, game),
+						value: game._id,
+					};
+				})}
+				onChange={(e) => {
+					setGame(e.value);
+				}}
+				className={classes.spaced}
+			/>
+			<Typography variant="h6">{t("notes.character.you")}</Typography>
+			<Select
+				options={characters.map((character) => {
+					return {
+						label: dbLocale(language, character),
+						value: character._id,
+					};
+				})}
+				onChange={(e) => {
+					setMyCharacter(e.value);
+				}}
+				className={classes.spaced}
+			/>
+			<Typography variant="h6">{t("notes.character.opponent")}</Typography>
+			<Select
+				options={characters.map((character) => {
+					return {
+						label: dbLocale(language, character),
+						value: character._id,
+					};
+				})}
+				onChange={(e) => {
+					setOpponentCharacter(e.value);
+				}}
+				className={classes.spaced}
+			/>
+			<Typography variant="h6">{t("notes.filter.choose")}</Typography>
+			<Select
+				options={filters.map((x) => {
+					return {
+						label: dbLocale(language, x),
+						value: x._id,
+					};
+				})}
+				onChange={(e) => {
+					setMyFilter(e.value);
+				}}
+				className={classes.spaced}
+			/>
+			{myFilter !== "" && (
+				<Button
+					variant="outlined"
+					color="secondary"
+					onClick={() => {
+						setMyFilter("");
+					}}
+				>
+					{t("notes.filter.clear")}
+				</Button>
+			)}
+		</>
+	);
 }
