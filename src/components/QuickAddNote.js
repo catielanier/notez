3cabbdf1fe1fc5dbde6ce09159ce577f81bc39d3
Corey Lanier <iam@coreylanier.com>
2021-10-16 15:37:36 -0400

    refactor i18n on quick add

--- src/components/QuickAddNote.js
+++ src/components/QuickAddNote.js
@@ -1,113 +1,105 @@
 import React, { useState, useContext } from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import { useTranslation } from "react-i18next";
 import {
-  Typography,
-  TextField,
-  Checkbox,
-  FormControlLabel,
-  Button
+	Typography,
+	TextField,
+	Checkbox,
+	FormControlLabel,
+	Button,
 } from "@material-ui/core";
 import Select from "react-select";
-import localeSelect from "../services/localeSelect";
-import {
-  quickAdd,
-  universalNote,
-  newNoteFilter,
-  newNote,
-  createNote
-} from "../data/locales";
 import dbLocale from "../services/dbLocale";
 import { LanguageContext } from "../contexts/LanguageContext";
 import { NoteContext } from "../contexts/NoteContext";
 
-const useStyles = makeStyles(theme => ({
-  spaced: {
-    marginBottom: theme.spacing(2)
-  }
+const useStyles = makeStyles((theme) => ({
+	spaced: {
+		marginBottom: theme.spacing(2),
+	},
 }));
 
 export default function QuickAddNote(props) {
-  const classes = useStyles();
-  const { language } = useContext(LanguageContext);
-  const { postNote, loading, error } = useContext(NoteContext);
-  const [note, setNote] = useState("");
-  const [filter, setFilter] = useState("");
-  const [universal, setUniversal] = useState(false);
+	const { t } = useTranslation();
+	const classes = useStyles();
+	const { language } = useContext(LanguageContext);
+	const { postNote, loading, error } = useContext(NoteContext);
+	const [note, setNote] = useState("");
+	const [filter, setFilter] = useState("");
+	const [universal, setUniversal] = useState(false);
 
-  const toggleUniversal = () => {
-    setUniversal(!universal);
-  };
+	const toggleUniversal = () => {
+		setUniversal(!universal);
+	};
 
-  return (
-    <div className="quick-add">
-      <Typography variant="h5" className={classes.spaced}>
-        {localeSelect(language, quickAdd)}
-      </Typography>
-      {error && (
-        <p>
-          <span>Error:</span> {error}
-        </p>
-      )}
-      {props.type === "Game Note" && (
-        <FormControlLabel
-          control={
-            <Checkbox
-              checked={universal}
-              onChange={toggleUniversal}
-              value={universal}
-              color="primary"
-            />
-          }
-          label={localeSelect(language, universalNote)}
-          className={classes.spaced}
-        />
-      )}
-      <Typography variant="h6">
-        {localeSelect(language, newNoteFilter)}
-      </Typography>
-      <Select
-        options={props.filters.map(filter => {
-          return {
-            label: dbLocale(language, filter),
-            value: filter._id
-          };
-        })}
-        onChange={e => {
-          setFilter(e.target.value);
-        }}
-        className={classes.spaced}
-      />
-      <Typography variant="h6">{localeSelect(language, newNote)}</Typography>
-      <TextField
-        multiline
-        value={note}
-        onChange={e => {
-          setNote(e.target.value);
-        }}
-        fullWidth
-        className={classes.spaced}
-      />
-      <Button
-        onClick={() => {
-          if (props.type === "Game Note") {
-            postNote(
-              props.type,
-              props.game,
-              props.opponentCharacter,
-              filter,
-              note,
-              props.myCharacter,
-              universal
-            );
-          } else if (props.type === "Player Note") {
-            postNote(props.type, props.game, props.player, filter, note);
-          }
-        }}
-        variant="contained"
-        color="primary"
-      >
-        {localeSelect(language, createNote)}
-      </Button>
-    </div>
-  );
+	return (
+		<div className="quick-add">
+			<Typography variant="h5" className={classes.spaced}>
+				{t("notes.common.quickAdd")}
+			</Typography>
+			{error && (
+				<p>
+					<span>Error:</span> {error}
+				</p>
+			)}
+			{props.type === "Game Note" && (
+				<FormControlLabel
+					control={
+						<Checkbox
+							checked={universal}
+							onChange={toggleUniversal}
+							value={universal}
+							color="primary"
+						/>
+					}
+					label={t("notes.common.universal")}
+					className={classes.spaced}
+				/>
+			)}
+			<Typography variant="h6">{t("notes.new.filter")}</Typography>
+			<Select
+				options={props.filters.map((filter) => {
+					return {
+						label: dbLocale(language, filter),
+						value: filter._id,
+					};
+				})}
+				onChange={(e) => {
+					setFilter(e.target.value);
+				}}
+				className={classes.spaced}
+			/>
+			<Typography variant="h6">{t("notes.new.note")}</Typography>
+			<TextField
+				multiline
+				value={note}
+				onChange={(e) => {
+					setNote(e.target.value);
+				}}
+				fullWidth
+				className={classes.spaced}
+			/>
+			<Button
+				onClick={() => {
+					if (props.type === "Game Note") {
+						postNote(
+							props.type,
+							props.game,
+							props.opponentCharacter,
+							filter,
+							note,
+							props.myCharacter,
+							universal
+						);
+					} else if (props.type === "Player Note") {
+						postNote(props.type, props.game, props.player, filter, note);
+					}
+				}}
+				variant="contained"
+				color="primary"
+			>
+				{t("notes.new.submit")}
+			</Button>
+		</div>
+	);
 }
--- src/components/VerifyUser.js
+++ src/components/VerifyUser.js
@@ -3,34 +3,34 @@ import { Redirect } from "react-router-dom";
 import axios from "axios";
 
 class VerifyUser extends React.Component {
-  state = {
-    success: false,
-    error: null
-  };
-  async componentWillMount() {
-    const key = window.location.pathname.replace("/verify/", "");
-    await axios
-      .post("/api/users/verify", { key })
-      .then(_ => {
-        this.setState({
-          success: true
-        });
-      })
-      .catch(error => {
-        this.setState({
-          error
-        });
-      });
-  }
-  render() {
-    if (this.state.success) {
-      if (this.state.success) {
-        return <Redirect to="/login" />;
-      }
-    } else {
-      return <section>Error: No Valid token.</section>;
-    }
-  }
+	state = {
+		success: false,
+		error: null,
+	};
+	async componentWillMount() {
+		const key = window.location.pathname.replace("/verify/", "");
+		await axios
+			.post("/api/users/verify", { key })
+			.then((_) => {
+				this.setState({
+					success: true,
+				});
+			})
+			.catch((error) => {
+				this.setState({
+					error,
+				});
+			});
+	}
+	render() {
+		if (this.state.success) {
+			if (this.state.success) {
+				return <Redirect to="/login" />;
+			}
+		} else {
+			return <section>Error: No Valid token.</section>;
+		}
+	}
 }
 
 export default VerifyUser;
