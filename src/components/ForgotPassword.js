a4dcbea0b7e24f610ce329789fe846ba40349ced
Corey Lanier <iam@coreylanier.com>
2021-11-07 16:32:09 -0500

    refactor reset password i18n

--- src/components/ForgotPassword.js
+++ src/components/ForgotPassword.js
@@ -1,118 +1,110 @@
 import React, { useState, useContext } from "react";
 import { Link as RouterLink } from "react-router-dom";
 import {
-  TextField,
-  Button,
-  Typography,
-  Container,
-  CircularProgress,
-  makeStyles,
+	TextField,
+	Button,
+	Typography,
+	Container,
+	CircularProgress,
+	makeStyles,
 } from "@material-ui/core";
-import localeSelect from "../services/localeSelect";
-import {
-  email as emailLocale,
-  goBack,
-  forgotPassword,
-  checkEmailPassword,
-  requestReset as requestResetLocale,
-} from "../data/locales";
 import { UserContext } from "../contexts/UserContext";
-import { LanguageContext } from "../contexts/LanguageContext";
+import { useTranslation } from "react-i18next";
 
 const useStyles = makeStyles((theme) => ({
-  container: {
-    display: "flex",
-    flexWrap: "wrap",
-  },
-  header: {
-    textAlign: "center",
-  },
-  buttonRow: {
-    display: "flex",
-    alignItems: "center",
-    marginTop: theme.spacing(2),
-  },
-  wrapper: {
-    margin: theme.spacing(1),
-    position: "relative",
-  },
-  buttonProgress: {
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    marginTop: -12,
-    marginLeft: -12,
-  },
+	container: {
+		display: "flex",
+		flexWrap: "wrap",
+	},
+	header: {
+		textAlign: "center",
+	},
+	buttonRow: {
+		display: "flex",
+		alignItems: "center",
+		marginTop: theme.spacing(2),
+	},
+	wrapper: {
+		margin: theme.spacing(1),
+		position: "relative",
+	},
+	buttonProgress: {
+		position: "absolute",
+		top: "50%",
+		left: "50%",
+		marginTop: -12,
+		marginLeft: -12,
+	},
 }));
 
 export default function ForgotPassword() {
-  const classes = useStyles();
-  const { loading, error, success, requestReset } = useContext(UserContext);
-  const { language } = useContext(LanguageContext);
-  const [email, setEmail] = useState("");
-  return (
-    <section>
-      <Container maxWidth="sm">
-        <Typography className={classes.header} variant="h5">
-          {localeSelect(language, forgotPassword)}
-        </Typography>
-        <form
-          disabled={loading}
-          onSubmit={(e) => {
-            e.preventDefault();
-            requestReset(email);
-          }}
-          className={classes.container}
-        >
-          <Container maxWidth="sm">
-            {success && <p>{localeSelect(language, checkEmailPassword)}</p>}
-            {error && (
-              <p className="error">
-                <span>Error:</span> {error}
-              </p>
-            )}
-            <Container maxWidth="sm">
-              <TextField
-                label={localeSelect(language, emailLocale)}
-                id="standard-name"
-                value={email}
-                onChange={(e) => {
-                  setEmail(e.target.value);
-                }}
-                fullWidth
-              />
-            </Container>
-            <Container className={classes.buttonRow}>
-              <div className={classes.wrapper}>
-                <Button
-                  variant="contained"
-                  type="submit"
-                  color="primary"
-                  disabled={loading}
-                >
-                  {localeSelect(language, requestResetLocale)}
-                </Button>
-                {loading && (
-                  <CircularProgress
-                    size={20}
-                    color="secondary"
-                    className={classes.buttonProgress}
-                  />
-                )}
-              </div>
-              <div className={classes.wrapper}>
-                <Button
-                  component={React.forwardRef((props, ref) => (
-                    <RouterLink innerRef={ref} to="/login" {...props} />
-                  ))}
-                >
-                  {localeSelect(language, goBack)}
-                </Button>
-              </div>
-            </Container>
-          </Container>
-        </form>
-      </Container>
-    </section>
-  );
+	const { t } = useTranslation();
+	const classes = useStyles();
+	const { loading, error, success, requestReset } = useContext(UserContext);
+	const [email, setEmail] = useState("");
+	return (
+		<section>
+			<Container maxWidth="sm">
+				<Typography className={classes.header} variant="h5">
+					{t("account.forgot")}
+				</Typography>
+				<form
+					disabled={loading}
+					onSubmit={(e) => {
+						e.preventDefault();
+						requestReset(email);
+					}}
+					className={classes.container}
+				>
+					<Container maxWidth="sm">
+						{success && <p>{t("account.checkEmail")}</p>}
+						{error && (
+							<p className="error">
+								<span>{t("common.error")}</span> {error}
+							</p>
+						)}
+						<Container maxWidth="sm">
+							<TextField
+								label={t("account.email")}
+								id="standard-name"
+								value={email}
+								onChange={(e) => {
+									setEmail(e.target.value);
+								}}
+								fullWidth
+							/>
+						</Container>
+						<Container className={classes.buttonRow}>
+							<div className={classes.wrapper}>
+								<Button
+									variant="contained"
+									type="submit"
+									color="primary"
+									disabled={loading}
+								>
+									{t("account.requestReset")}
+								</Button>
+								{loading && (
+									<CircularProgress
+										size={20}
+										color="secondary"
+										className={classes.buttonProgress}
+									/>
+								)}
+							</div>
+							<div className={classes.wrapper}>
+								<Button
+									component={React.forwardRef((props, ref) => (
+										<RouterLink innerRef={ref} to="/login" {...props} />
+									))}
+								>
+									{t("common.goBack")}
+								</Button>
+							</div>
+						</Container>
+					</Container>
+				</form>
+			</Container>
+		</section>
+	);
 }
