6ce5aa896ca13d0e7be62d9f2ef6c8ae6c53654b
Corey Lanier <iam@coreylanier.com>
2021-11-07 16:36:18 -0500

    refactor reset password i18n

--- src/components/ResetPassword.js
+++ src/components/ResetPassword.js
@@ -1,114 +1,110 @@
 import React, { useContext, useState } from "react";
 import { Redirect } from "react-router-dom";
 import {
-  TextField,
-  Button,
-  Typography,
-  Container,
-  CircularProgress,
-  makeStyles
+	TextField,
+	Button,
+	Typography,
+	Container,
+	CircularProgress,
+	makeStyles,
 } from "@material-ui/core";
-import localeSelect from "../services/localeSelect";
-import {
-  resetPassword as resetPasswordLocale,
-  password as passwordLocale,
-  verifyPassword as verifyPasswordLocale,
-  noMatch
-} from "../data/locales";
-import { LanguageContext } from '../contexts/LanguageContext';
-import { UserContext } from '../contexts/UserContext';
+import { UserContext } from "../contexts/UserContext";
+import { useTranslation } from "react-i18next";
 
-const useStyles = makeStyles(theme => ({
-  container: {
-    display: "flex",
-    flexWrap: "wrap"
-  },
-  header: {
-    textAlign: "center"
-  },
-  buttonRow: {
-    display: "flex",
-    alignItems: "center",
-    marginTop: theme.spacing(2)
-  },
-  wrapper: {
-    margin: theme.spacing(1),
-    position: "relative"
-  },
-  buttonProgress: {
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    marginTop: -12,
-    marginLeft: -12
-  }
+const useStyles = makeStyles((theme) => ({
+	container: {
+		display: "flex",
+		flexWrap: "wrap",
+	},
+	header: {
+		textAlign: "center",
+	},
+	buttonRow: {
+		display: "flex",
+		alignItems: "center",
+		marginTop: theme.spacing(2),
+	},
+	wrapper: {
+		margin: theme.spacing(1),
+		position: "relative",
+	},
+	buttonProgress: {
+		position: "absolute",
+		top: "50%",
+		left: "50%",
+		marginTop: -12,
+		marginLeft: -12,
+	},
 }));
 
 export default function ResetPassword() {
-  const classes = useStyles();
-  const { language } = useContext(LanguageContext);
-  const { success, error, loading, resetPassword } = useContext(UserContext);
-  const [password, setPassword] = useState('');
-  const [verifyPassword, setVerifyPassword] = useState('');
-  const token = window.location.pathname.replace("/forgot/", "");
-  if (success) {
-    return <Redirect to="/login" />
-  }
-  return (
-    <section className="signup">
-      <Container maxWidth="sm">
-        <Typography className={classes.header} variant="h5">
-          {localeSelect(language, resetPasswordLocale)}
-        </Typography>
-        <form disabled={loading} onSubmit={e => {
-            resetPassword(password, verifyPassword, token)
-          }}>
-          {error && (
-            <p className="error">
-              <span>Error:</span> {error}
-            </p>
-          )}
-          <TextField
-            label={localeSelect(language, passwordLocale)}
-            required
-            onChange={e => {
-              setPassword(e.target.value)
-            }}
-            fullWidth
-            value={password}
-            type="password"
-          />
-          <TextField
-            label={localeSelect(language, verifyPassword)}
-            required
-            onChange={e => {
-              setVerifyPassword(e.target.value)
-            }}
-            fullWidth
-            value={verifyPassword}
-            type="password"
-          />
-          <Container className={classes.buttonRow}>
-            <div className={classes.wrapper}>
-              <Button
-                color="primary"
-                variant="contained"
-                type="submit"
-                disabled={loading}
-              >
-                {localeSelect(language, resetPasswordLocale)}
-              </Button>
-              {loading && (
-                <CircularProgress
-                  size={20}
-                  color="secondary"
-                  className={classes.buttonProgress}
-                />
-              )}
-            </div>
-          </Container>
-        </form>
-      </Container>
-    </section>
-  )
+	const { t } = useTranslation();
+	const classes = useStyles();
+	const { success, error, loading, resetPassword } = useContext(UserContext);
+	const [password, setPassword] = useState("");
+	const [verifyPassword, setVerifyPassword] = useState("");
+	const token = window.location.pathname.replace("/forgot/", "");
+	if (success) {
+		return <Redirect to="/login" />;
+	}
+	return (
+		<section className="signup">
+			<Container maxWidth="sm">
+				<Typography className={classes.header} variant="h5">
+					{t("account.reset")}
+				</Typography>
+				<form
+					disabled={loading}
+					onSubmit={(e) => {
+						resetPassword(password, verifyPassword, token);
+					}}
+				>
+					{error && (
+						<p className="error">
+							<span>{t("common.error")}</span> {error}
+						</p>
+					)}
+					<TextField
+						label={t("account.password")}
+						required
+						onChange={(e) => {
+							setPassword(e.target.value);
+						}}
+						fullWidth
+						value={password}
+						type="password"
+					/>
+					<TextField
+						label={t("account.verify")}
+						required
+						onChange={(e) => {
+							setVerifyPassword(e.target.value);
+						}}
+						fullWidth
+						value={verifyPassword}
+						type="password"
+					/>
+					<Container className={classes.buttonRow}>
+						<div className={classes.wrapper}>
+							<Button
+								color="primary"
+								variant="contained"
+								type="submit"
+								disabled={loading}
+							>
+								{t("account.reset")}
+							</Button>
+							{loading && (
+								<CircularProgress
+									size={20}
+									color="secondary"
+									className={classes.buttonProgress}
+								/>
+							)}
+						</div>
+					</Container>
+				</form>
+			</Container>
+		</section>
+	);
 }
