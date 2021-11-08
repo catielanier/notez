35f38599f61fe28342a35614dbb23510ccec248a
Corey Lanier <iam@coreylanier.com>
2021-11-07 21:30:20 -0500

    refactor user settings i18n

--- src/components/UserSettings.js
+++ src/components/UserSettings.js
@@ -2,115 +2,103 @@ import React, { useContext, useState, useEffect } from "react";
 import axios from "axios";
 import Select from "react-select";
 import {
-  Container,
-  Button,
-  Typography,
-  Radio,
-  RadioGroup,
-  FormControlLabel
+	Container,
+	Button,
+	Typography,
+	Radio,
+	RadioGroup,
+	FormControlLabel,
 } from "@material-ui/core";
 import { Redirect } from "react-router-dom";
 import { getToken } from "../services/tokenService";
-import localeSelect from "../services/localeSelect";
-import {
-  editUserRoles,
-  selectUser,
-  assignRole,
-  user as userLocale,
-  admin,
-  banned,
-  updateUser
-} from "../data/locales";
-import { UserContext } from '../contexts/UserContext';
-import { LanguageContext } from '../contexts/LanguageContext';
+import { UserContext } from "../contexts/UserContext";
+import { useTranslation } from "react-i18next";
 
 export default function UserSettings() {
-  const { user, loading, success, error, updateRole } = useContext(UserContext);
-  const { language } = useContext(LanguageContext);
-  const [users, setUsers] = useState([]);
-  const [selectedUser, setSelectedUser] = useState('');
-  const [role, setRole] = useState('');
-  useEffect(() => {
-    const token = getToken();
-    async function fetchData() {
-      await axios
-      .get("/api/users", {
-        params: {
-          token,
-          user
-        }
-      })
-      .then(res => {
-        setUsers(res.data.data);
-      });
-    }
-    fetchData();
-  }, [user])
-  if (!user) {
-    return <Redirect to="/" />
-  }
-  return (
-    <section>
-      <Container maxWidth="sm">
-        <Typography variant="h5" gutterBottom>
-          {localeSelect(language, editUserRoles)}
-        </Typography>
-        <Typography variant="h6">
-          {localeSelect(language, selectUser)}
-        </Typography>
-        <Select
-          options={users.map(user => {
-            return {
-              label: `${user.username} (${user.realName} - ${user.country})`,
-              value: user._id
-            };
-          })}
-          onChange={e => {
-            setSelectedUser(e.value);
-            const index = users.findIndex(x => x._id === e.value);
-            setRole(users[index].role);
-          }}
-        />
-      {selectedUser !== "" && role !== "" && (
-          <>
-            <Typography variant="h6">
-              {localeSelect(language, assignRole)}
-            </Typography>
-            <RadioGroup
-              onChange={e => {
-                setRole(e.target.value)
-              }}
-              value={role}
-            >
-              <FormControlLabel
-                value="User"
-                control={<Radio color="primary" />}
-                label={localeSelect(language, userLocale)}
-              />
-              <FormControlLabel
-                value="Admin"
-                control={<Radio color="primary" />}
-                label={localeSelect(language, admin)}
-              />
-              <FormControlLabel
-                value="Banned"
-                control={<Radio color="primary" />}
-                label={localeSelect(language, banned)}
-              />
-            </RadioGroup>
-            <Button
-              color="primary"
-              variant="contained"
-              onClick={e => {
-                e.preventDefault();
-                updateRole(selectedUser, role);
-              }}
-            >
-              {localeSelect(language, updateUser)}
-            </Button>
-          </>
-        )}
-      </Container>
-    </section>
-  );
+	const t = useTranslation();
+	const { user, loading, success, error, updateRole } = useContext(UserContext);
+	const [users, setUsers] = useState([]);
+	const [selectedUser, setSelectedUser] = useState("");
+	const [role, setRole] = useState("");
+	useEffect(() => {
+		const token = getToken();
+		async function fetchData() {
+			await axios
+				.get("/api/users", {
+					params: {
+						token,
+						user,
+					},
+				})
+				.then((res) => {
+					setUsers(res.data.data);
+				});
+		}
+		fetchData();
+	}, [user]);
+	if (!user) {
+		return <Redirect to="/" />;
+	}
+	return (
+		<section>
+			<Container maxWidth="sm">
+				<Typography variant="h5" gutterBottom>
+					{t("settings.user.role.edit")}
+				</Typography>
+				<Typography variant="h6">{t("settings.user.select")}</Typography>
+				<Select
+					options={users.map((user) => {
+						return {
+							label: `${user.username} (${user.realName} - ${user.country})`,
+							value: user._id,
+						};
+					})}
+					onChange={(e) => {
+						setSelectedUser(e.value);
+						const index = users.findIndex((x) => x._id === e.value);
+						setRole(users[index].role);
+					}}
+				/>
+				{selectedUser !== "" && role !== "" && (
+					<>
+						<Typography variant="h6">
+							{t("settings.user.role.assign")}
+						</Typography>
+						<RadioGroup
+							onChange={(e) => {
+								setRole(e.target.value);
+							}}
+							value={role}
+						>
+							<FormControlLabel
+								value="User"
+								control={<Radio color="primary" />}
+								label={t("settings.user.role.user")}
+							/>
+							<FormControlLabel
+								value="Admin"
+								control={<Radio color="primary" />}
+								label={t("settings.user.role.admin")}
+							/>
+							<FormControlLabel
+								value="Banned"
+								control={<Radio color="primary" />}
+								label={t("settings.user.role.banned")}
+							/>
+						</RadioGroup>
+						<Button
+							color="primary"
+							variant="contained"
+							onClick={(e) => {
+								e.preventDefault();
+								updateRole(selectedUser, role);
+							}}
+						>
+							{t("settings.user.update")}
+						</Button>
+					</>
+				)}
+			</Container>
+		</section>
+	);
 }
