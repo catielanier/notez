68a818a9c876c9319b639cdf04780de5fcdf9d1c
Corey Lanier <iam@coreylanier.com>
2021-11-09 16:56:37 -0500

    fix country translations

--- src/contexts/CountryContext.js
+++ src/contexts/CountryContext.js
@@ -12,54 +12,53 @@ const CountryContextProvider = (props) => {
 		await axios.get(`https://restcountries.com/v3.1/all`).then((res) => {
 			const unsortedCountries = res.data;
 			const countryData = unsortedCountries.map((country) => {
-				switch (i18n.language) {
-					case "ko":
+				if (i18n.language.indexOf("ko") === 0) {
+					return {
+						label: country.translations.kor.common,
+						value: country.cca3,
+					};
+				} else if (i18n.language.indexOf("ja") === 0) {
+					return {
+						label: country.translations.jpn.common,
+						value: country.cca3,
+					};
+				} else if (i18n.language.indexOf("zh") === 0) {
+					if (country.name.nativeName.zho) {
 						return {
-							label: country.translations.kor.common,
+							label: country.name.nativeName.zho.common,
 							value: country.cca3,
 						};
-					case "ja":
+					} else {
 						return {
-							label: country.translations.jpn.common,
+							label: country.translations.zho.common,
 							value: country.cca3,
 						};
-					case "zh":
-						if (country.name.nativeName.zho) {
-							return {
-								label: country.name.nativeName.zho.common,
-								value: country.cca3,
-							};
-						} else {
-							return {
-								label: country.translations.zho.common,
-								value: country.cca3,
-							};
-						}
-					case "es":
-						return {
-							label: country.translations.spa.common,
-							value: country.cca3,
-						};
-					case "pt":
-						return {
-							label: country.translations.por.common,
-							value: country.cca3,
-						};
-					case "it":
-						return {
-							label: country.translations.ita.common,
-							value: country.cca3,
-						};
-					case "ru":
-						return {
-							label: country.translations.rus.common,
-							value: country.cca3,
-						};
-					default:
-						return {
-							label: country.name.common,
-							value: country.cca3,
-						};
+					}
+				} else if (i18n.language.indexOf("es") === 0) {
+					return {
+						label: country.translations.spa.common,
+						value: country.cca3,
+					};
+				} else if (i18n.language.indexOf("pt") === 0) {
+					return {
+						label: country.translations.por.common,
+						value: country.cca3,
+					};
+				} else if (i18n.language.indexOf("it") === 0) {
+					return {
+						label: country.translations.ita.common,
+						value: country.cca3,
+					};
+				} else if (i18n.language.indexOf("ru") === 0) {
+					return {
+						label: country.translations.rus.common,
+						value: country.cca3,
+					};
+				} else {
+					return {
+						label: country.name.common,
+						value: country.cca3,
+					};
 				}
 			});
 			countryData.sort((a, b) => {
