8e98a8889d4618886d67db726f799fbbf120caf8
Corey Lanier <iam@coreylanier.com>
2022-04-06 00:12:23 -0400

    update models to consume keys instead of 15 different names

--- api/routes/characters/characterModel.js
+++ api/routes/characters/characterModel.js
@@ -3,37 +3,15 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 const characterSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  name_ja: {
-    type: String,
-  },
-  name_ko: {
-    type: String,
-  },
-  "name_zh-cn": {
-    type: String,
-  },
-  "name_zh-tw": {
-    type: String,
-  },
-  name_it: {
-    type: String,
-  },
-  name_es: {
-    type: String,
-  },
-  name_pt: {
-    type: String,
-  },
-  name_ru: {
-    type: String,
-  },
-  name_fr: {
-    type: String,
-  },
+	name: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	company: {
+		type: String,
+		required: true,
+	},
 });
 
 exports.model = mongoose.model("Character", characterSchema);
