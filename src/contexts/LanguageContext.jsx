55f39519c8bdc7e20823610258b3b8272e6e4d78
Catie Lanier <hello@catielanier.ca>
2025-06-17 21:26:33 -0400

    update language context for i18n changes

--- src/contexts/LanguageContext.jsx
+++ src/contexts/LanguageContext.jsx
@@ -1,25 +1,29 @@
-import React, { useState, createContext, useEffect } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const LanguageContext = createContext();
 
 const LanguageContextProvider = (props) => {
-	const [language, setLanguage] = useState(null);
-	useEffect(() => {
-		const locale = navigator.language;
-		const index = locale.indexOf("zh");
-		if (index !== 0) {
-			const userLanguage = locale.split(/[-_]/)[0];
-			setLanguage(userLanguage);
-		} else {
-			const userLanguage = locale;
-			setLanguage(userLanguage);
-		}
-	});
-	return (
-		<LanguageContext.Provider value={{ language }}>
-			{props.children}
-		</LanguageContext.Provider>
-	);
+  const [language, setLanguage] = useState(null);
+  useEffect(() => {
+    const locale = navigator.language;
+    const index = locale.indexOf("zh");
+    if (index !== 0) {
+      const userLanguage = locale.split(/[-_]/)[0];
+      setLanguage(userLanguage);
+    } else {
+      if (locale === "zh-CN") {
+        const userLanguage = locale;
+        setLanguage(userLanguage);
+      } else {
+        setLanguage("zh");
+      }
+    }
+  });
+  return (
+    <LanguageContext.Provider value={{ language }}>
+      {props.children}
+    </LanguageContext.Provider>
+  );
 };
 
 export default LanguageContextProvider;
